8c33f155d04b0638c46ce1620dd84b44249aac92
麦子_today <maizi_today@sina.com>
2022-03-21 21:36:06 +0800

    提交

--- src/main/java/com/maizi/workflow/entity/Student.java
+++ src/main/java/com/maizi/workflow/entity/Student.java
@@ -4,9 +4,11 @@
  * @Author: yubo
  * @Date: 2022-03-13 11:39:19
  * @LastEditors: yubo
- * @LastEditTime: 2022-03-13 15:23:05
+ * @LastEditTime: 2022-03-15 12:32:27
  */
 package com.maizi.workflow.entity;
+
+import java.util.Date;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -31,5 +33,11 @@ public class Student {
     private String sex;
 
     private Integer age;
+
+    private Date date;
+
+    public static void main(String[] args) {
+
+    }
 
 }
